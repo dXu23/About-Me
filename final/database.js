d37c7ac6c62442294d7b2c68998f142f438a0f75
Daniel Xu <dxu23@uic.edu>
2017-11-28 23:26:55 +0000

    Got Dexie.js to work the way I want

--- final/database.js
+++ final/database.js
@@ -2,36 +2,67 @@
 //Declare the database
 var db = new Dexie("promise_DB");
 db.version(1).stores({
-    //Promise Name and the datetime it is due
+    //Promise Name, datetime it is due, and the priority of promise
     promises: "++id, name, datetime, priority" 
-})
+});
 
 
-function log(txt) {
-    $("#pending").append("<li>" + txt + "</li>");
+function toHTML(name, date) {
+    return "<li class = 'mdc-list-item>" + "<span class = 'mdc-list-item__text'>" + name 
+    + "</span>" + "<span class = 'mdc-list-item__text__secondary'>" + date + "</span>" + "</li>";
+}
+
+function render(objStore, mode) {
+    var scontent = "";
+    objStore.orderBy(mode).each(function(promise) {
+        scontent += toHTML(promise.name, promise.datetime);
+    }).then(function() {
+        console.log(scontent);
+        $("#pending").html(scontent);
+    }).catch(function(error) {
+        console.log("Error in function render: " + error);
+    })
 }
 
 $(document).ready(function() {
-    db.open().then(function() {
-        console.log("Appending stored data to Pending Promsies tab...").then(function() {
-        db.promises.orderBy("datetime").each(promise => log(promise.name));
-        });
-    })
+    // add onSumbmit event so that when button with id submitPromise is clicked, 
+    // function onSumbit is called
     
-    //When button is clicked, open the database and add user's input data
-    $("#submitPromise").click(function() {
+    db.open().then(function() {
+        render(db.promises, "datetime");
+    }).catch(function(error) {
+        console.log("Error in initial render...:" + error);
+    });
+    
+    // onSubmit function
+    function onSubmit() {
         //Open the database
         // Add the promise name and its due date to the database
         db.open().then(function() {
-            console.log($("#promiseName").val());
-            console.log($("#dateTime").val());
-            db.promises.add({name: $("#promiseName").val(), datetime: new Date($("#dateTime").val())}).then(function() {
-                log(JSON.stringify(db.promises, null, 2));
+            // Sanity checkers: 
+            //console.log("promiseName: " + $("#promiseName").val());
+            //console.log("dateTime: " + $("#dateTime").val());
+            // Append the promise to the objectStore promiseList which is in the database promiseDB
+            var html;
+            db.promises.put({name: $("#promiseName").val(), datetime: $("#dateTime").val()}).then(function() {
+                render(db.promises, "datetime");
+                /*
+                db.promises.orderBy("dateTime").each(function(item) {
+                    html += "<li class = 'mdc-list-item'>" + item.name + "</li>";
+                }).then(function() {
+                    console.log(html);
+                    $("#pending").html(html);
+                    */
+            }).catch(function(error) {
+                console.log("Error:" + error);
             });
         }).catch((reason) => {
             console.log("Reason why failed: " + reason);
         });
         console.log("Good night, world!");
-    });
+    }
+    
+    document.getElementById("submitPromise").addEventListener("click", onSubmit);
+
     
 });
